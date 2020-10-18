5ef8d52a4ec1780ea332d0eec7abcc84e0ac85ee
cchsMartin <54595803+cchsMartin@users.noreply.github.com>
2020-10-18 10:36:49 -0400

    Update EmailAddress.java

--- EmailAddress.java
+++ EmailAddress.java
@@ -14,14 +14,15 @@ public class Unit1Project {
 		System.out.println("What is your last name?");
 		String lastName = keyboard.nextLine();
 		
-		System.out.println("What is your birthdate? Please enter in this format: mmddyyyy");
+		System.out.println("What is your birthdate? Please enter in this format: mmddyyyy"); //THIS SHOULD BE MM-DD-YYYY
 		String birthdate = keyboard.nextLine();
 
 			
 		System.out.println("Email: " + lastName.toLowerCase() + firstName.substring(0,1) + middleName.substring(0,1) + "@sharkmail.com");
-		System.out.println("Temp Password: " + birthdate);
+		System.out.println("Temp Password: " + birthdate); //SHOULD BE THE BIRTHDAY PROCESSED
 		System.out.println("*** This temporary password is very insecure.Please change it immediately! ***");
 		
 
 	}
 }
+// 90/100 Resubmit with proposed changes for a 100.
